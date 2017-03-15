8b0bfe09b2446c395c1fe6943a7d2a8963dc4e96
Way911 <wayne911@live.cn>
2017-03-16 02:55:51 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ docker rm -f ss_server && echo "ss_server removed" || echo "no ss_server contain
 
 docker build -t ss_server .
 
-docker run -d --network=host ss_server
+docker run -d --name ss_server --network=host ss_server
