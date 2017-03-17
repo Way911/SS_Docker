681414af65198472ef01f9f46f805db792fe5d9a
Wayne Cao <wayne911@live.cn>
2017-03-17 16:08:15 +0800

    update

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/usr/bi/env bash
 
-docker rm -f ss_server && echo "ss_server removed" || echo "no ss_server container"
+docker rm -f ss_mac && echo "ss_mac removed" || echo "no ss_mac container"
+docker run -dt --name ss_mac -p 80:80 mritd/shadowsocks -s "-s 0.0.0.0 -p 80 -m aes-256-cfb -k password --fast-open"
 
-docker build -t ss_server .
+docker rm -f ss_ip && echo "ss_ip removed" || echo "no ss_ip container"
+docker run -dt --name ss_ip -p 443:443 mritd/shadowsocks -s "-s 0.0.0.0 -p 443 -m aes-256-cfb -k password --fast-open"
 
-docker run -d --name ss_server -p 80:80 -p 443:443 -p 8080:8080 -p 81:81 ss_server
+docker rm -f ss_pc && echo "ss_pc removed" || echo "no ss_pc container"
+docker run -dt --name ss_pc -p 8080:8080 mritd/shadowsocks -s "-s 0.0.0.0 -p 8080 -m aes-256-cfb -k password --fast-open"
+
