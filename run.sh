996aa9fff342399907a09a8d71ff035d1b7d6474
Wayne Cao <wayne911@live.cn>
2019-01-12 16:44:42 +0800

    update shell for centos

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bi/env bash
-
-docker rm -f ss_server && echo "ss_server removed" || echo "no ss_server container"
-
-docker build -t ss_server .
-
-docker run -d --name ss_server -p 80:80 -p 443:443 -p 8080:8080 -p 81:81 ss_server
