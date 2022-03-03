0c1ab2b3c00ffdc7ac4c76cbe7b0628655713511
Way911 <wayne911@live.cn>
2022-03-03 12:29:14 +0800

    Update Trojan.sh

--- Trojan.sh
+++ Trojan.sh
@@ -1,210 +1,69 @@
 #!/bin/bash
 
-#fonts color
-yellow(){
-    echo -e "\033[33m\033[01m$1\033[0m"
-}
-green(){
-    echo -e "\033[32m\033[01m$1\033[0m"
-}
-red(){
-    echo -e "\033[31m\033[01m$1\033[0m"
-}
-
-#copy from qsyb ss scripts
-if [[ -f /etc/redhat-release ]]; then
-    release="centos"
-    systemPackage="yum"
-    systempwd="/usr/lib/systemd/system/"
-elif cat /etc/issue | grep -Eqi "debian"; then
-    release="debian"
-    systemPackage="apt-get"
-    systempwd="/lib/systemd/system/"
-elif cat /etc/issue | grep -Eqi "ubuntu"; then
-    release="ubuntu"
-    systemPackage="apt-get"
-    systempwd="/lib/systemd/system/"
-elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
-    release="centos"
-    systemPackage="yum"
-    systempwd="/usr/lib/systemd/system/"
-elif cat /proc/version | grep -Eqi "debian"; then
-    release="debian"
-    systemPackage="apt-get"
-    systempwd="/lib/systemd/system/"
-elif cat /proc/version | grep -Eqi "ubuntu"; then
-    release="ubuntu"
-    systemPackage="apt-get"
-    systempwd="/lib/systemd/system/"
-elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
-    release="centos"
-    systemPackage="yum"
-    systempwd="/usr/lib/systemd/system/"
+#仅仅搞centos7
+if [ ! -e '/etc/redhat-release' ]; then
+echo "仅支持centos7"
+exit
+fi
+if  [ -n "$(grep ' 6\.' /etc/redhat-release)" ] ;then
+echo "仅支持centos7"
+exit
 fi
 
-function install_trojan(){
-CHECK=$(grep SELINUX= /etc/selinux/config | grep -v "#")
-if [ "$CHECK" == "SELINUX=enforcing" ]; then
-    red "======================================================================="
-    red "detect SELinux is Enabled，please reboot VPS first"
-    red "======================================================================="
-    read -p "Reboot now ?Please input [Y/n] :" yn
-	[ -z "${yn}" ] && yn="y"
-	if [[ $yn == [Yy] ]]; then
-	    sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
-            setenforce 0
-	    echo -e "VPS rebooting..."
-	    reboot
-	fi
-    exit
-fi
-if [ "$CHECK" == "SELINUX=permissive" ]; then
-    red "======================================================================="
-    red "detect SELinux is in permissive status，please reboot VPS first"
-    red "======================================================================="
-    read -p "reboot now? please input [Y/n] :" yn
-	[ -z "${yn}" ] && yn="y"
-	if [[ $yn == [Yy] ]]; then
-	    sed -i 's/SELINUX=permissive/SELINUX=disabled/g' /etc/selinux/config
-            setenforce 0
-	    echo -e "VPS rebooting..."
-	    reboot
-	fi
-    exit
-fi
-if [ "$release" == "centos" ]; then
-    if  [ -n "$(grep ' 6\.' /etc/redhat-release)" ] ;then
-    red "==============="
-    red "current OS is not supportted"
-    red "==============="
-    exit
-    fi
-    if  [ -n "$(grep ' 5\.' /etc/redhat-release)" ] ;then
-    red "==============="
-    red "current OS is not supportted"
-    red "==============="
-    exit
-    fi
-    systemctl stop firewalld
-    systemctl disable firewalld
-    rpm -Uvh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
-elif [ "$release" == "ubuntu" ]; then
-    if  [ -n "$(grep ' 14\.' /etc/os-release)" ] ;then
-    red "==============="
-    red "current OS is not supportted"
-    red "==============="
-    exit
-    fi
-    if  [ -n "$(grep ' 12\.' /etc/os-release)" ] ;then
-    red "==============="
-    red "current OS is not supportted"
-    red "==============="
-    exit
-    fi
-    systemctl stop ufw
-    systemctl disable ufw
-    apt-get update
-fi
-$systemPackage -y install  nginx wget unzip zip curl tar >/dev/null 2>&1
-systemctl enable nginx.service
-green "======================="
-yellow "Please input DNS name which is pointing to this VPS"
-green "======================="
-read your_domain
-real_addr=`ping ${your_domain} -c 1 | sed '1{s/[^(]*(//;s/).*//;q}'`
-local_addr=`curl ipv4.icanhazip.com`
-if [ $real_addr == $local_addr ] ; then
-	green "=========================================="
-	green "       DNS name resolving is OK，start installing trojan"
-	green "=========================================="
-	sleep 1s
-cat > /etc/nginx/nginx.conf <<-EOF
-user  root;
-worker_processes  1;
-error_log  /var/log/nginx/error.log warn;
-pid        /var/run/nginx.pid;
-events {
-    worker_connections  1024;
+install_docker(){
+
+	yum remove -y docker docker-client docker-client-latest docker-common docker-latest  docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine		
+	yum install -y yum-utils device-mapper-persistent-data lvm2
+	yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+	yum makecache fast
+	yum -y install docker-ce
+	systemctl start docker
+	systemctl enable docker
+
 }
-http {
-    include       /etc/nginx/mime.types;
-    default_type  application/octet-stream;
-    log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
-                      '\$status \$body_bytes_sent "\$http_referer" '
-                      '"\$http_user_agent" "\$http_x_forwarded_for"';
-    access_log  /var/log/nginx/access.log  main;
-    sendfile        on;
-    #tcp_nopush     on;
-    keepalive_timeout  120;
-    client_max_body_size 20m;
-    #gzip  on;
-    server {
-        listen       80;
-        server_name  $your_domain;
-        root /usr/share/nginx/html;
-        index index.php index.html index.htm;
-    }
+
+config_website(){
+
+	cd /usr/src/trojan/web
+	wget https://github.com/atrandys/trojan/raw/master/index.zip
+	unzip index.zip
+
 }
-EOF
-	#setup fake website
-	rm -rf /usr/share/nginx/html/*
-	cd /usr/share/nginx/html/
-	wget https://github.com/V2RaySSR/Trojan/raw/master/web.zip
-    	unzip web.zip
-	systemctl restart nginx.service
-	#applying https certification
-	mkdir /usr/src/trojan-cert
-	curl https://get.acme.sh | sh
-	~/.acme.sh/acme.sh  --issue  -d $your_domain  --webroot /usr/share/nginx/html/ --register-account -m my@example.com
-    	~/.acme.sh/acme.sh  --installcert  -d  $your_domain   \
-        --key-file   /usr/src/trojan-cert/private.key \
-        --fullchain-file /usr/src/trojan-cert/fullchain.cer \
-        --reloadcmd  "systemctl force-reload  nginx.service"
-	if test -s /usr/src/trojan-cert/fullchain.cer; then
-        cd /usr/src
-	#wget https://github.com/trojan-gfw/trojan/releases/download/v1.13.0/trojan-1.13.0-linux-amd64.tar.xz
-	wget https://github.com/trojan-gfw/trojan/releases/download/v1.14.0/trojan-1.14.0-linux-amd64.tar.xz
-	tar xf trojan-1.*
-	#downloading trojan client
-	wget https://github.com/atrandys/trojan/raw/master/trojan-cli.zip
-	unzip trojan-cli.zip
-	cp /usr/src/trojan-cert/fullchain.cer /usr/src/trojan-cli/fullchain.cer
-	trojan_passwd=$(cat /dev/urandom | head -1 | md5sum | head -c 8)
-	cat > /usr/src/trojan-cli/config.json <<-EOF
-{
-    "run_type": "client",
-    "local_addr": "127.0.0.1",
-    "local_port": 1080,
-    "remote_addr": "$your_domain",
-    "remote_port": 443,
-    "password": [
-        "$trojan_passwd"
-    ],
-    "log_level": 1,
-    "ssl": {
-        "verify": true,
-        "verify_hostname": true,
-        "cert": "fullchain.cer",
-        "cipher_tls13":"TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384",
-	"sni": "",
-        "alpn": [
-            "h2",
-            "http/1.1"
-        ],
-        "reuse_session": true,
-        "session_ticket": false,
-        "curves": ""
-    },
-    "tcp": {
-        "no_delay": true,
-        "keep_alive": true,
-        "fast_open": false,
-        "fast_open_qlen": 20
-    }
+
+uninstall_trojan(){
+	docker update --restart=no trojan
+	docker stop trojan
+	docker rm trojan
+	rm -rf /usr/src/trojan/
+	echo "================="
+	echo "    卸载完成"
+	echo "================="
 }
-EOF
-	rm -rf /usr/src/trojan/server.conf
-	cat > /usr/src/trojan/server.conf <<-EOF
+
+config_trojan(){
+
+yum -y install  wget unzip vim tcl expect expect-devel
+mkdir /usr/src/trojan
+mkdir /usr/src/trojan/web
+cd /usr/src/trojan
+read -p "输入你的VPS绑定的域名：" domain
+SUBJECT="/C=US/ST=Mars/L=iTranswarp/O=iTranswarp/OU=iTranswarp/CN=$domain"
+echo "============================"
+echo " 接下来需要设定密码，输入两次（随意设置，5-10位）"
+echo "============================"
+openssl genrsa -des3 -out private.key 1024
+echo "============================"
+echo " 接下来需要输入刚设定的密码"
+echo "============================"
+openssl req -new -subj $SUBJECT -key private.key -out private.csr
+echo "============================"
+echo " 再次输入刚设定的密码"
+echo "============================"
+mv private.key private.or.key
+openssl rsa -in private.or.key -out private.key
+openssl x509 -req -days 3650 -in private.csr -signkey private.key -out private.crt
+
+cat > /usr/src/trojan/server.conf <<-EOF
 {
     "run_type": "server",
     "local_addr": "0.0.0.0",
@@ -212,15 +71,15 @@ EOF
     "remote_addr": "127.0.0.1",
     "remote_port": 80,
     "password": [
-        "$trojan_passwd"
+        "password1"
     ],
     "log_level": 1,
     "ssl": {
-        "cert": "/usr/src/trojan-cert/fullchain.cer",
-        "key": "/usr/src/trojan-cert/private.key",
+        "cert": "/usr/src/trojan/private.crt",
+        "key": "/usr/src/trojan/private.key",
         "key_password": "",
-        "cipher_tls13":"TLS_AES_128_GCM_SHA256:TLS_CHACHA20_POLY1305_SHA256:TLS_AES_256_GCM_SHA384",
-	"prefer_server_cipher": true,
+        "cipher": "ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256",
+        "prefer_server_cipher": true,
         "alpn": [
             "http/1.1"
         ],
@@ -247,122 +106,56 @@ EOF
     }
 }
 EOF
-	cd /usr/src/trojan-cli/
-	zip -q -r trojan-cli.zip /usr/src/trojan-cli/
-	trojan_path=$(cat /dev/urandom | head -1 | md5sum | head -c 16)
-	mkdir /usr/share/nginx/html/${trojan_path}
-	mv /usr/src/trojan-cli/trojan-cli.zip /usr/share/nginx/html/${trojan_path}/
-	#installing startup scripts
-	
-cat > ${systempwd}trojan.service <<-EOF
-[Unit]  
-Description=trojan  
-After=network.target  
-   
-[Service]  
-Type=simple  
-PIDFile=/usr/src/trojan/trojan/trojan.pid
-ExecStart=/usr/src/trojan/trojan -c "/usr/src/trojan/server.conf"  
-ExecReload=  
-ExecStop=/usr/src/trojan/trojan  
-PrivateTmp=true  
-   
-[Install]  
-WantedBy=multi-user.target
-EOF
 
-	chmod +x ${systempwd}trojan.service
-	systemctl start trojan.service
-	systemctl enable trojan.service
-	green "======================================================================"
-	green "Trojan installation completed.please click following URL to download trojan client. the client has been configured with all params"
-	green "1. copy following URL , open it in browser to download client"
-	yellow "http://${your_domain}/$trojan_path/trojan-cli.zip"
-	red "please take note of following redirection rules for trojan client"
-	yellow "http://${your_domain}/trojan.txt"
-	green "2.please unzip the client package you downloaded. open folder. execute start.bat to run Trojan client"
-	green "3. execute stop.bat to stop Trojan client"
-	green "4. Trojan client can be working with browser plugins like switchyomega"
-	green "you can also visit https://www.v2rayssr.com/trojan-1.html ‎to download plugins"
-	green "======================================================================"
-	else
-        red "================================"
-	red "failed to apply https certification. installation failed"
-	red "================================"
-	fi
-	
-else
-	red "================================"
-	red "the address of DNS name resolve does not match VPS IP"
-	red "installation failed. please ensure DNS name resolve is working. Please ensure DNS name and IP is correct"
-	red "================================"
-fi
+echo "============================"
+echo " 设置验证密码，服务端和客户端使用相同密码"
+echo "============================"
+read -p "设置密码：" mypassword
+sed -i "s/password1/$mypassword/" /usr/src/trojan/server.conf
+
 }
 
-function remove_trojan(){
-    red "================================"
-    red "removing trojan"
-    red "removing nginx"
-    red "================================"
-    systemctl stop trojan
-    systemctl disable trojan
-    rm -f ${systempwd}trojan.service
-    if [ "$release" == "centos" ]; then
-        yum remove -y nginx
-    else
-        apt autoremove -y nginx
-    fi
-    rm -rf /usr/src/trojan*
-    rm -rf /usr/share/nginx/html/*
-    green "=============="
-    green "trojan deleted"
-    green "=============="
-}
+start_docker(){
 
-function bbr_boost_sh(){
-    bash <(curl -L -s -k "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh")
+        sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
+	sudo firewall-cmd --zone=public --add-port=443/tcp --permanent
+	sudo firewall-cmd --reload
+	docker run --name trojan --restart=always -d -p 80:80 -p 443:443 -v /usr/src/trojan:/usr/src/trojan  atrandys/trojan sh -c "/etc/init.d/nginx start && trojan -c /usr/src/trojan/server.conf"
+	echo "============================"
+	echo "       trojan启动完成"
+	echo "============================"
 }
 
 start_menu(){
     clear
-    green " ===================================="
-    green " Trojan installation scripts      "
-    green " OS: centos7+/debian9+/ubuntu16.04+"
-    green " Link: www.v2rayssr.com   "
-    green " This scripts is originated by atrandys, integrated with BBRPLUS"
-    green " Youtube：https://www.youtube.com/channel/UCoHcnzcwjsFRUi6SkKq7Gbg"
-    green " ===================================="
+    echo "========================="
+    echo " 介绍：适用于CentOS7"
+    echo " 作者：A"
+    echo "========================="
+    echo "1. 安装Trojan"
+    echo "2. 卸载Trojan"
+    echo "3. 退出"
     echo
-    red " ===================================="
-    yellow " 1. install Trojan"
-    red " ===================================="
-    yellow " 2. install 4 IN 1 BBRPLUS"
-    red " ===================================="
-    yellow " 3. remove Trojan"
-    red " ===================================="
-    yellow " 0. exit"
-    red " ===================================="
-    echo
-    read -p "Please input number:" num
+    read -p "请输入数字:" num
     case "$num" in
-    1)
-    install_trojan
-    ;;
-    2)
-    bbr_boost_sh 
-    ;;
-    3)
-    remove_trojan
-    ;;
-    0)
-    exit 1
-    ;;
-    *)
-    clear
-    red "Please input correct number"
-    sleep 1s
-    start_menu
-    ;;
+    	1)
+	install_docker
+	config_trojan
+	config_website
+	start_docker
+	;;
+	2)
+	uninstall_trojan
+	;;
+	3)
+	exit 1
+	;;
+	*)
+	clear
+	echo "请输入正确数字"
+	sleep 5s
+	start_menu
+	;;
     esac
 }
 
