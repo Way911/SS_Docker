6cadf2dc3a492fc9f96617f76ba297b0f4119ad4
Way911 <wayne911@live.cn>
2022-10-28 13:22:17 +0800

    Update NaiveProxy.sh

--- NaiveProxy.sh
+++ NaiveProxy.sh
@@ -1,3 +1,4 @@
+# https://www.youtube.com/watch?v=TY5MmBry1yw
 yum install wget -y
 # 如果是debian系统是apt install wget
 wget https://raw.githubusercontent.com/imajeason/nas_tools/main/NaiveProxy/install.sh
