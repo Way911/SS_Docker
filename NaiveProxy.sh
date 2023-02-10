de0d109d93b5512942e5f3de58ff30a7b8ffb46c
Way911 <wayne911@live.cn>
2023-02-10 13:18:49 +0800

    Update NaiveProxy.sh

--- NaiveProxy.sh
+++ NaiveProxy.sh
@@ -2,4 +2,5 @@
 yum install wget -y
 # 如果是debian系统是apt install wget
 wget https://raw.githubusercontent.com/imajeason/nas_tools/main/NaiveProxy/install.sh
+mkdir -p /etc/letsencrypt/live/
 bash install.sh
