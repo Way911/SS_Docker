73ac127d78edd04e24398676b0f48da1e6bfcc86
Way911 <wayne911@live.cn>
2023-09-22 12:30:45 +0800

    Update az_cli.sh

--- az_cli.sh
+++ az_cli.sh
@@ -1,2 +1,2 @@
-# https://learn.microsoft.com/en-us/azure/container-apps/vnet-custom?tabs=bash%2Cazure-cli&pivots=azure-cli
+
 az container create --resource-group ss --name ss --image shadowsocks/shadowsocks-libev --ports 8388 --environment-variables PASSWORD=March2022! --cpu 1 --memory 1
