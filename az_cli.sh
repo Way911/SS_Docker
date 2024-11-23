1848bceef56ec328428c1ffe3f726034a353ed9c
Way911 <wayne911@live.cn>
2024-11-23 09:33:10 +0800

    Update az_cli.sh

--- az_cli.sh
+++ az_cli.sh
@@ -1,3 +1,4 @@
+#az container show --name ss --resource-group ss --query ipAddress.ip --output tsv
 #az container show --name ss --resource-group ss --output tsv
 #az container delete --ids /subscriptions/xxxxxx/resourceGroups/ss/providers/Microsoft.ContainerInstance/containerGroups/ss
 az container create --resource-group ss --name ss --image shadowsocks/shadowsocks-libev --ports 8388 --environment-variables PASSWORD=March2022! --cpu 1 --memory 1 --dns-name-label sscontainer
